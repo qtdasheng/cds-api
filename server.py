f42b289d72fbc9e403f20b7d5cfb808fa832d3e8
Lishaoeng111 <1593073434@qq.com>
2020-02-29 19:10:18 +0800

    docker部署

--- server.py
+++ server.py
@@ -4,9 +4,8 @@ from apiapp.views import user_api
 from apiapp import app
 from apiapp.views import user_api
 
+app.register_blueprint(user_api.blue, url_prefix='/api/')
 
 if __name__ == '__main__':
-    # 注册蓝图
-    app.register_blueprint(user_api.blue, url_prefix='/api/')
-
+    # 注册蓝图  
     app.run(host='0.0.0.0', port=5000)
